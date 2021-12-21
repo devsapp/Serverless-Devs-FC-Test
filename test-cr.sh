005c87eb82103f6bb5b34e48248dccf7eb2219fb
rsonghuster <ls_huster@163.com>
2021-12-21 17:51:06 +0800

    split custom and custom-containere

--- test-cr.sh
+++ test-cr.sh
@@ -2,17 +2,9 @@
 set -x
 rm -rf ~/.s/components/devsapp.cn/fc@dev
 
-# Test custom container
-cd custom-container
-echo "test custom-container runtime ..."
-rm -rf .s
-s build -d
-s local invoke -e '{"hello":"fc"}'
-s deploy -y --use-local
-s invoke -e '{"hello":"fc"}'
 
 # Test custom runtime
-cd ../custom
+cd custom
 cd python
 
 echo "test custom python runtime event function ..."
