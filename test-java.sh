15df97b3b5aa33dab09af753df0f98a5019b9b67
wss-git <wssgryx@163.com>
2022-11-07 10:20:33 +0800

    test

--- test-java.sh
+++ test-java.sh
@@ -3,8 +3,8 @@ set -x
 set -e
 s clean --all
 
-export FC_DOCKER_VERSION=1.10.4 
-# export core_load_serverless_devs_component='devsapp/domain@dev;devsapp/fc-deploy@dev'
+# export FC_DOCKER_VERSION=1.10.4 
+export core_load_serverless_devs_component='devsapp/fc@dev;devsapp/fc-info@dev;devsapp/fc-deploy@dev'
 
 # Test Java Runtime
 echo "test java8 runtime ..."
