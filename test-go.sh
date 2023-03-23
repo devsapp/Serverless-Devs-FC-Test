a5924f07e60f15c34e0718135fe0dea945669f64
wss-git <wssgryx@163.com>
2023-03-23 13:13:06 +0800

    test: 测试10。7 版本

--- test-go.sh
+++ test-go.sh
@@ -3,8 +3,8 @@ set -x
 set -e
 s clean --all
 
-export FC_DOCKER_VERSION=1.10.7
-# export core_load_serverless_devs_component='devsapp/fc@dev;devsapp/fc-deploy@dev'
+# export FC_DOCKER_VERSION=1.10.7
+export core_load_serverless_devs_component='devsapp/fc-core@dev;devsapp/fc-deploy@dev;devsapp/fc-build@dev;devsapp/fc-local-invoke@dev'
 
 # Test go1.x Runtime
 cd go
