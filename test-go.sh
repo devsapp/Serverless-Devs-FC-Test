54a43ad5106fb3acdd63a3ab523e1f5faf2093f7
wss <wssgryx@163.com>
2023-03-29 13:21:09 +0800

    test: FC_DOCKER_VERSION=1.10.8
    Signed-off-by: wss <wssgryx@163.com>

--- test-go.sh
+++ test-go.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 s clean --all
 
-# export FC_DOCKER_VERSION=1.10.7
+export FC_DOCKER_VERSION=1.10.8
 export core_load_serverless_devs_component='devsapp/fc-core@dev;devsapp/fc-deploy@dev;devsapp/fc-build@dev;devsapp/fc-local-invoke@dev'
 
 # Test go1.x Runtime
