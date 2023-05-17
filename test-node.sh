155f4be39e1015c0ec5932b403720ef6b6736d87
wss <wssgryx@163.com>
2023-05-17 11:25:17 +0800

    test: 测试
    Signed-off-by: wss <wssgryx@163.com>

--- test-node.sh
+++ test-node.sh
@@ -3,8 +3,8 @@ set -x
 set -e
 s clean --all
 
-export FC_DOCKER_VERSION=1.10.8
-# export core_load_serverless_devs_component='devsapp/fc-core@dev;devsapp/fc-deploy@dev;devsapp/fc-build@dev;devsapp/fc-local-invoke@dev'
+# export FC_DOCKER_VERSION=1.10.8
+export core_load_serverless_devs_component='devsapp/fc-core@dev;devsapp/fc-deploy@dev;devsapp/fc@dev;devsapp/nas@dev'
 
 # Test Nodjs Runtime
 cd nodejs
