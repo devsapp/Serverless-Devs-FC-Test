d98ab5501b12ff41a45e7129702f11c653de4d52
wss <wssgryx@163.com>
2023-05-17 14:37:34 +0800

    test: 2023-05-17 version
    Signed-off-by: wss <wssgryx@163.com>

--- test-php.sh
+++ test-php.sh
@@ -4,7 +4,7 @@ set -e
 s clean --all
 
 # export FC_DOCKER_VERSION=1.10.8
-export core_load_serverless_devs_component='devsapp/fc-core@dev;devsapp/fc-deploy@dev;devsapp/fc@dev;devsapp/nas@dev'
+# export core_load_serverless_devs_component='devsapp/fc-core@dev;devsapp/fc-deploy@dev;devsapp/fc@dev;devsapp/nas@dev'
 
 # Test Php7.2 Runtime
 cd php
--- test-windows.sh
+++ test-windows.sh
@@ -4,7 +4,7 @@ set -e
 # s clean --component
 
 # export FC_DOCKER_VERSION=1.10.8
-export core_load_serverless_devs_component='devsapp/fc-core@dev;devsapp/fc-deploy@dev;devsapp/fc@dev;devsapp/nas@dev'
+# export core_load_serverless_devs_component='devsapp/fc-core@dev;devsapp/fc-deploy@dev;devsapp/fc@dev;devsapp/nas@dev'
 
 # Test go1.x Runtime
 cd go
